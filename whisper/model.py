70916afc5b166764edb8a24279f241976051ef1c
dunky11 <timv94@zedat.fu-berlin.de>
2022-11-27 16:45:35 +0100

    print shape for debug

--- whisper/model.py
+++ whisper/model.py
@@ -96,6 +96,7 @@ class MultiHeadAttention(nn.Module):
         if mask is not None:
             qk = qk + mask[:n_ctx, :n_ctx]
 
+        print(qk.shape)
         w = F.softmax(qk.float(), dim=-1).to(q.dtype)
         return (w @ v).permute(0, 2, 1, 3).flatten(start_dim=2)
 
